9bb86536f2c7d580c9c81b63107bb999ba37a8e2
皮蛋瘦肉粥 <77768660+vf19961226@users.noreply.github.com>
2021-03-22 23:44:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import pandas as pd
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, TimeDistributed
-#from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
 import argparse
